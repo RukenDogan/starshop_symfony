d51d350afadae96e8c6de1e1064efabc05f37f02
rukenDogan <ruken.dogan.rkn@gmail.com>
2025-12-10 16:23:13 +0100

    cache rules everything around me

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\ItemInterface;
@@ -17,10 +18,10 @@ class MainController extends AbstractController
     public function homepage(
         StarshipRepository $starshipRepository,
         HttpClientInterface $client,
-        CacheInterface $cache,
-        CacheInterface $issLocationPool,
+        #[Autowire(param: 'iss_location_cache_ttl')]
+        $issLocationCacheTtl,
     ): Response {
-
+        dd($this->getParameter('iss_location_cache_ttl'));
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
