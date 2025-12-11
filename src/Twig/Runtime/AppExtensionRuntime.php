0593f5f4d9b16425e3de7144231970bbd955f32a
rukenDogan <ruken.dogan.rkn@gmail.com>
2025-12-11 12:02:02 +0100

    Add Twig function for real-time ISS data in base template

---
+++ src/Twig/Runtime/AppExtensionRuntime.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Twig\Runtime;
+
+use Twig\Extension\RuntimeExtensionInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class AppExtensionRuntime implements RuntimeExtensionInterface
+{
+    public function __construct(
+        private readonly HttpClientInterface $client,
+        private readonly CacheInterface $issLocationPool
+    ) {
+
+    }
+
+    public function getIssLocationData(): array
+    {
+        return $this->issLocationPool->get('iss_location_data', function (): array {
+            $response = $this->client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+            return $response->toArray();
+        });
+    }
+}
